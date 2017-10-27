429fe6da7da321926fead614bb0edd4ef2458652
orbitcoder <brawat.mail@gmail.com>
2017-10-27 12:24:56 +0530

    done

--- app/src/main/java/loglibs/com/logslibexample/MainActivity.java
+++ app/src/main/java/loglibs/com/logslibexample/MainActivity.java
@@ -3,11 +3,14 @@ package loglibs.com.logslibexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import loglibs.com.logslib.LogsLib;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LogsLib.d(new String[]{"anything"});
     }
 }
---
+++ logslib/src/main/java/loglibs/com/logslib/LogsLib.java
@@ -0,0 +1,15 @@
+package loglibs.com.logslib;
+
+/**
+ * Created by bhupendra on 27/10/17.
+ */
+
+public class LogsLib {
+    public static void d(String[] args) {
+        for (String arg : args) {
+            System.out.print(args);
+        }
+
+
+    }
+}
